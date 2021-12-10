77e3a9390cfe42e7255574bc244e39267a316d0b
Brenton Weaver <brenton.weaver@gmail.com>
2021-12-10 19:06:43 +1100

    created a test site because i was struggling to understand the launch code for the button, once I have a better idea of what is happening ill copy working code iinto the script.js file

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
+// // Write password to the #password input
+// function writePassword() {
   
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 function generatePassword () {
-  var requestedLength = prompt("How long do you want your password to be? /n Password must be between 8 - 128 characters.");
+  var passwordLength = window.prompt("How long do you want your password to be? /n Password must be between 8 - 128 characters.");
 }
 
-//pseudo code
+generatePassword();
 
-prompt("How long do you want your password to be? Password must be between 8 - 128 characters.")
-
-
-//create and array that include the data sets "upper/lower/special"===== May learn a better way to write an array??/
-
-var upperCaseSet = "'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'";
-var lowerCaseSet = "'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z'";
-var specialCaseSet = "'~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '`', '-', '+', '{', '}', '|', '[', ']', '\', ':', '<', '>', '?', ',' ,'. ',' /' ";
-
-//create random value generator that has a var that a user can input password length into
-//create a an error for "<8" and ">128"
-
-if (passwordLength <8 ||
-  passwordLength <128 || isNaN) {
-    alert ("The response you entered is invalid," /n + "please enter a number between 8 and 128.")
-  }
-
-// make boolean statements for "use upper/lower/special characters:"
-
-confirm("Would you like to include Uppercase characters?")
-
-confirm("Would you like to include Lower characters?")
-
-confirm("Would you like to include Special characters?")
+// //pseudo code
 
 
+// //create and array that include the data sets "upper/lower/special"===== May learn a better way to write an array??/
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// var upperCaseSet = "'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'";
+// var lowerCaseSet = "'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z'";
+// var specialCaseSet = "'~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '`', '-', '+', '{', '}', '|', '[', ']', '\', ':', '<', '>', '?', ',' ,'. ',' /' ";
+
+// //create random value generator that has a var that a user can input password length into
+// //create a an error for "<8" and ">128"
+
+// if (passwordLength <8 ||
+//   passwordLength <128 || isNaN) {
+//     alert ("The response you entered is invalid," /n + "please enter a number between 8 and 128.")
+//   }
+
+// // make boolean statements for "use upper/lower/special characters:"
+
+// confirm("Would you like to include Uppercase characters?")
+
+// confirm("Would you like to include Lower characters?")
+
+// confirm("Would you like to include Special characters?")
+
+
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
---
+++ testZone/script2.js
@@ -0,0 +1,11 @@
+function generatePassword () {
+    var passwordLength = prompt("How long do you want your password to be? /n Password must be between 8 - 128 characters.");
+  }
+  
+  generatePassword();
+  console.log(passwordLength);
+
+  if (passwordLength <8 ||
+  passwordLength <128 || isNaN) {
+    alert ("The response you entered is invalid," /n + "please enter a number between 8 and 128.")  
+}
