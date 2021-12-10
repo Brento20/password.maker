d1bee5853193b4cf5f799fce8410d90862ba4c34
Brenton Weaver <brenton.weaver@gmail.com>
2021-12-10 22:18:12 +1100

    made a little progress in the test script

--- testZone/script2.js
+++ testZone/script2.js
@@ -1,11 +1,28 @@
 function generatePassword () {
-    var passwordLength = prompt("How long do you want your password to be? /n Password must be between 8 - 128 characters.");
-  }
+    passwordLength = prompt("How long do you want your password to be? /n Password must be between 8 - 128 characters.");
+    var lengthConfirm = parseInt(passwordLength);
+    }
   
-  generatePassword();
-  console.log(passwordLength);
 
-  if (passwordLength <8 ||
-  passwordLength <128 || isNaN) {
-    alert ("The response you entered is invalid," /n + "please enter a number between 8 and 128.")  
+function setRequirements () {
+   var hasUpper = confirm("Would you like to include uppercase characters? For NO please select CANCEL.")
+   var hasLower = confirm("Would you like to include Lowercase characters For NO please select CANCEL.?")
+   var hasSpecial = confirm("Would you like to include Special characters? For NO please select CANCEL.")
+   var hasNumber = confirm("Would you like to include Numbers? For NO please select CANCEL.")
 }
+  
+
+
+generatePassword();
+
+
+console.log(passwordLength);
+
+  
+setRequirements ();
+
+console.log(setRequirements);
+console.log(hasUpper);
+
+// if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+//     alert ("Choice is invalid, please select a password length between 8 - 128.");
