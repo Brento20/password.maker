f7a9e090068283ce34c6539235630279b582f699
Brenton Weaver <brenton.weaver@gmail.com>
2021-12-11 21:15:04 +1100

    Gave up on trying to push charaters into the charaterPool array with a preassigned variable and just am pussing the individual values

--- testZone/testzone.js
+++ testZone/testzone.js
@@ -24,37 +24,31 @@ passwordErrorChecker();
 var characterPool = [];
 var hasUpper = "'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'";
 var hasLower = "'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z'";
-var hasSpecial = "'~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '`', '-', '+', '{', '}', '|', '[', ']', '\', ':', '<', '>', '?', ',' ,'. ',' /' ";
+var hasSpecial = "'~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '`', '-', '+', '{', '}', '|', '[', ']', '\', ':', '<', '>', '?', ',', '.', ',' '/' ";
 var hasNumber = "'1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ";
 
-//========== Boolean variables to select characters ===========
+//========== Boolean variables to select characters that push the character sets into an array called characterPool if they have a true response ===========
   
 var hasUpper = confirm("Would you like to include Uppercase characters?");
 if (hasUpper == true) {
-    for (var i = 0; i < hasUpper.length; i++) {
-        characterPool.push(hasUpper[i]);
-    }
+    characterPool.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
 }
 
 var hasLower = confirm("Would you like to include Lowercase characters?");
 if (hasLower == true) {
-    for (var i = 0; i < hasLower.length; i++) {
-        characterPool.push(hasLower[i]);
-    }
+    characterPool.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 }
+
 
 var hasSpecial = confirm("Would you like to include Special characters?");
 if (hasSpecial == true) {
-    for (var i = 0; i < hasSpecial.length; i++) {
-        characterPool.push(hasSpecial[i]);
-    }
+    characterPool.push('~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '`', '-', '+', '{', '}', '|', '[', ']');
+
 }
 
 var hasNumber = confirm("Would you like to include Numeric characters?");
 if (hasNumber == true) {
-    for (var i = 0; i < hasNumber.length; i++) {
-        characterPool.push(hasNumber[i]);
-    }
+    characterPool.push('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
 }
 
 //========== Consol log to check details and help me set up the next function ===========
@@ -64,7 +58,7 @@ if (hasNumber == true) {
     console.log(hasLower)
     console.log(hasSpecial)
     console.log(hasNumber)
+    console.log(characterPool)
 
-// Next steps
+// Next steps - use math.random in a for loop to pull out characters from the character pool and pass them onto a new variable for the complete password
 
-// merge arrays for character sets that have a true boolean and use a for loop 
