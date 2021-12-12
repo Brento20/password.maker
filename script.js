a3ca2582d1461b810a7cf3cc9c6fe5ae9bc59f64
Brenton Weaver <brenton.weaver@gmail.com>
2021-12-12 23:08:37 +1100

    fixed by rechecking the curly brackets, it closed too early and wasnt running half of the code

--- script.js
+++ script.js
@@ -9,54 +9,56 @@ function writePassword() {
     passwordText.value = password;
   }
 
-  function generatePassword () {
-    passwordLength = prompt("How long do you want your password to be? Password must be between 8 - 128 characters.");
-    var passwordLength = parseInt(passwordLength);
+      function generatePassword () {
+        passwordLength = prompt("How long do you want your password to be? Password must be between 8 - 128 characters.");
+        var passwordLength = parseInt(passwordLength);
+        
+        
+          // generatePassword();
+          //=============================
+      if (passwordLength <= 7) {
+          alert("Password too short, please try again!");
+          return "Press Generate Password to try again";
+      }  else if (passwordLength >= 129) {
+          alert("Password too long, please try again!");
+          return "Press Generate Password to try again";
+      }  
+
+
+    var characterPool = []; // The empty array to be filled with characters as selected by user
+      
+        var hasUpper = confirm("Would you like to include Uppercase characters?"); // each will push into the above array (characterPool)
+        if (hasUpper == true) {
+            characterPool.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+        }
+
+        var hasLower = confirm("Would you like to include Lowercase characters?");
+        if (hasLower == true) {
+            characterPool.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+        }
+
+
+        var hasSpecial = confirm("Would you like to include Special characters?");
+        if (hasSpecial == true) {
+            characterPool.push('~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '`', '-', '+', '{', '}', '|', '[', ']');
+        }
+
+        var hasNumber = confirm("Would you like to include Numeric characters?");
+        if (hasNumber == true) {
+            characterPool.push('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+        }
+
+    var newPassword = ""; //the below runs a for loop to fill each space in the passwordLength with a array item from characterPool then return pulls the result out of the function.
+    for (var i = 0; i < passwordLength; i++) {
+          characterPool[
+          Math.floor(Math.random() * characterPool.length)];
+      newPassword +=
+          characterPool[
+          Math.floor(Math.random() * characterPool.length)
+        ];
     }
-    
-    // generatePassword();
-    //=============================
-if (passwordLength <= 7) {
-    alert("Password too short, please try again!");
-    generatePassword();
-}  else if (passwordLength >= 129) {
-    alert("Password too long, please try again!");
-    generatePassword();
+ return newPassword;
 }
-
-
-var characterPool = [];
-  
-var hasUpper = confirm("Would you like to include Uppercase characters?");
-if (hasUpper == true) {
-    characterPool.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
-}
-
-var hasLower = confirm("Would you like to include Lowercase characters?");
-if (hasLower == true) {
-    characterPool.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
-}
-
-
-var hasSpecial = confirm("Would you like to include Special characters?");
-if (hasSpecial == true) {
-    characterPool.push('~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '`', '-', '+', '{', '}', '|', '[', ']');
-
-}
-
-var hasNumber = confirm("Would you like to include Numeric characters?");
-if (hasNumber == true) {
-    characterPool.push('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
-}
-
-for (var i = 0; i < passwordLength; i++) {
-  characterPool[
-      Math.floor(Math.random() * characterPool.length)];
-  Password +=
-     characterPool[
-         Math.floor(Math.random() * characterPool.length)
-     ];
- }
 
 
 // Add event listener to generate button
