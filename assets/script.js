dceb241d3195f98b7a9ec6c51e6880d1dc6641f7
Brenton Weaver <brenton.weaver@gmail.com>
2021-12-14 16:45:57 +1100

    cleaned up the indentation and tried to fix the else if statment to reject ABC chareters- didnt win that battle

--- assets/script.js
+++ assets/script.js
@@ -11,54 +11,55 @@ function writePassword() {
 
       function generatePassword () {
         passwordLength = prompt("How long do you want your password to be? Password must be between 8 - 128 characters.");
-        var passwordLength = parseInt(passwordLength);
+        // var passwordLength = parseInt(passwordLength);
         
         
        
           // This set of it statements is to make sure we get a valid input form the user
       if (passwordLength <= 7) {
           alert("Password too short, please try again!");
-          return "Press Generate Password to try again";
+            return "Press Generate Password to try again";
       }  else if (passwordLength >= 129) {
           alert("Password too long, please try again!");
-          return "Press Generate Password to try again";
-      // }  else if (passwordLength != NaN) {
-      //     alert("Password cannot contain letters, please try again!");
-      //     return "Press Generate Password to try again";
-      };
+            return "Press Generate Password to try again";
+      }
+    //   }    else if (passwordLength === isNaN) {
+    //       alert("Password cannot contain letters, please try again!");
+    //         return "Press Generate Password to try again";
+    //   };
           
 
 
     var characterPool = []; // The empty array to be filled with characters as selected by user
       
         var hasUpper = confirm("Would you like to include Uppercase characters?"); // each will push into the above array (characterPool)
-        if (hasUpper == true) {
-            characterPool.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+            if (hasUpper == true) {
+                characterPool.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
         }
 
         var hasLower = confirm("Would you like to include Lowercase characters?");
-        if (hasLower == true) {
-            characterPool.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+            if (hasLower == true) {
+                characterPool.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
         }
 
 
         var hasSpecial = confirm("Would you like to include Special characters?");
-        if (hasSpecial == true) {
-            characterPool.push('~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '`', '-', '+', '{', '}', '|', '[', ']');
+            if (hasSpecial == true) {
+                characterPool.push('~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '`', '-', '+', '{', '}', '|', '[', ']');
         }
 
         var hasNumber = confirm("Would you like to include Numeric characters?");
-        if (hasNumber == true) {
-            characterPool.push('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+            if (hasNumber == true) {
+                characterPool.push('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
         }
 
     var newPassword = ""; //the below runs a for loop to fill each space in the passwordLength with a array item from characterPool then return pulls the result out of the function.
     for (var i = 0; i < passwordLength; i++) {
           characterPool[
-          Math.floor(Math.random() * characterPool.length)];
+            Math.floor(Math.random() * characterPool.length)];
       newPassword +=
           characterPool[
-          Math.floor(Math.random() * characterPool.length)
+            Math.floor(Math.random() * characterPool.length)
         ];
     }
  return newPassword;
